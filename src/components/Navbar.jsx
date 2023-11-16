da2ce929948746ec2d787072886057b105273609
Pietro Rosano <pietrorosano.code@gmail.com>
2023-11-16 12:25:09 +0100

    flex-wrap navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="ms-auto my-2 my-lg-0 gap-5"
+            className="ms-auto my-2 my-lg-0 gap-5 flex-wrap"
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
